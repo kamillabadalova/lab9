6f78143cc813a5783e5d84252888ec5f1559e1fd
Kama <56154113+kamillabadalova@users.noreply.github.com>
2019-11-29 18:47:21 +0300

    Пункт 1 подпункт б)

---
+++ Main1.java
@@ -0,0 +1,15 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main1 extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+            Parent root = FXMLLoader.load(getClass().getResource("sample1.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+}
